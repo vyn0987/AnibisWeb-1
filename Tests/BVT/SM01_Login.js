f66cc21ae9d4b4cc4d2294e962b35fd300f757d8
U806426 <vy.nguyen@mobi.ch>
2019-10-18 14:03:50 +0700

    V test

--- Tests/BVT/SM01_Login.js
+++ Tests/BVT/SM01_Login.js
@@ -14,9 +14,9 @@ describe('SM01 - Login', function() {
   /**
   * TESTCASES
   */
-  it('Verify user can login successful with valid account', function() {
+  xit('Verify user can login successful with valid account', function() {
     this.accounts.forEach((account) => {
-      // Login with valid account
+      // Login with valid account;
       cy.login(account.email, account.password)
 
       // VP: username displays on my profile page
